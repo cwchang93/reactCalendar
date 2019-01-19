456ebcae4f6b578cbb5b81d80bce84c479335cdd
Jinwei <cwchang93@gmail.com>
2019-01-19 18:03:40 +0800

    leftClick bug

--- src/components/calendarHead.jsx
+++ src/components/calendarHead.jsx
@@ -18,12 +18,63 @@ class CalendarHead extends Component {
             // renderYearArr: ['2000', '2000', '2000'], // 畫面上要render的年 Arr [String, String, String ]
             renderYearArr: [], // 畫面上要render的年 Arr [String, String, String ]
             renderMonthArr: null, // 畫面上要render的月 Arr [String, String, String ]
+
             initYear: null, // 最初輸入的年部分
             initMonthAfterZero: null, // 最初輸入的月份(去掉0)
 
             preInitYear: null, // initYear的前面一格
             afterInitYear: null, // initYear的後面一格
+
+            preInitMonth: null, // initMonth的前面一格
+            afterInitMonth: null, // initMonth的前面一格
         };
+    }
+
+    getData(path) {
+        fetch(path)
+            .then((response) => {
+                return response.json();
+            })
+            .then((travelDataHead) =>
+                this.setState(
+                    {
+                        travelDataHead: travelDataHead,
+                        // }
+                    },
+                    () => {
+                        this.renderDataFunc(this.state.travelDataHead, 'date');
+                        console.log('hithere');
+                        // this.collectInitYearMonth();
+                        // this.handleMonthArr();
+                        // this.handleYearArr();
+                        // this.sortedDateFunc(this.state.unsortedArr);
+                        // console.log('this.state.unsortedArr');
+                        // console.log(this.state.unsortedArr);
+                        // console.log('travelDataHead in getData');
+                        // console.log(this.state.travelDataHead);
+                    }
+                )
+            )
+            .catch((error) => console.log('parsing failed', error));
+    }
+
+    componentDidMount() {
+        const {travelDataHead} = this.state;
+        // this.handleRenderYearMonthArr();
+        this.getData(this.props.path);
+        this.collectInitYearMonth();
+
+        // this.handleYearArr();
+        // this.handleMonthArr();
+        // this.renderDataFunc(travelDataHead);
+
+        // console.log('travelDataHead in DidMount');
+        // console.log(travelDataHead); // 這邊為何拿不到東西?
+        // console.log('this.state.oldestMonth in DidMount');
+        // console.log(this.state.oldestMonth);
+        // console.log(this.state.unsortedArr);
+
+    // this.sortedDateFunc(this.state.unsortedArr);
     }
 
     renderDataFunc(jsonData, jsonKey) {
@@ -53,6 +104,36 @@ class CalendarHead extends Component {
     // console.log('oldestYear', oldestYear); // 最左邊月份
     // console.log('newestMonth', newestMonth); // 最右邊月份
     // console.log('newestYear', newestYear); // 最右邊月份
+    }
+
+    collectInitYearMonth() {
+    // 把initYear跟Month切開
+        const {initYearMonth} = this.state;
+        const initYearMonthLen = initYearMonth.length;
+        const initYear = initYearMonth.substr(0, 4);
+        const initMonth = initYearMonth.substr(initYearMonthLen - 2, 2); // 只取後面兩個
+        let initMonthAfterZero;
+        // 如果initMonth第一個數字是0 ==>只取後面的數字
+
+        if (initMonth[0] == '0') {
+            //   initMonth = init[1];  // Uncaught Error: "initMonth" is read-only
+            initMonthAfterZero = initMonth[1];
+            // console.log(initMonthAfterZero);
+        } else {
+            initMonthAfterZero = initMonth;
+        }
+        console.log('initYearinitYear123', initYear);
+
+        console.log('initMonthAfterZero123', initMonthAfterZero);
+        this.setState(
+            {
+                initYear: initYear,
+                initMonthAfterZero: initMonthAfterZero,
+            },
+            () => {
+                this.handleYearArr(), this.handleMonthArr();
+            }
+        );
     }
 
     handleYearArr() {
@@ -116,142 +197,16 @@ class CalendarHead extends Component {
         }
     }
 
-    collectInitYearMonth() {
-    // 把initYear跟Month切開
-        const {initYearMonth} = this.state;
-        const initYearMonthLen = initYearMonth.length;
-        const initYear = initYearMonth.substr(0, 4);
-        const initMonth = initYearMonth.substr(initYearMonthLen - 2, 2); // 只取後面兩個
-        let initMonthAfterZero;
-        // 如果initMonth第一個數字是0 ==>只取後面的數字
-
-        if (initMonth[0] == '0') {
-            //   initMonth = init[1];  // Uncaught Error: "initMonth" is read-only
-            initMonthAfterZero = initMonth[1];
-            // console.log(initMonthAfterZero);
-        } else {
-            initMonthAfterZero = initMonth;
-        }
-        console.log('initYearinitYear123', initYear);
-
-        console.log('initMonthAfterZero123', initMonthAfterZero);
-        this.setState(
-            {
-                initYear: initYear,
-                initMonthAfterZero: initMonthAfterZero,
-            },
-            () => {
-                this.handleYearArr(), this.handleMonthArr();
-            }
-        );
-    }
-
-    // handleRenderYearMonthArr() {
-    //     console.log(initMonthAfterZero);
-    //     console.log(initYear);
-
-    //     // 年
-
-    //     // 放進要render(map)的arr
-    //     // ok 待處理:  如果輸入的月份是01 => premonth變12
-
-    //     //  月
-
-    //     this.setState({
-    //         renderMonthArr: renderMonthArr,
-    //         // renderYearArr: renderYearArr,
-    //         // initYear: initYear,
-    //         initMonthAfterZero: initMonthAfterZero,
-    //         preInitYear,
-    //         afterInitYear,
-    //     });
-
-    // // initYear,
-    // }
-
-    // 原本年月合起來的function  以下
-
-    // handleRenderYearMonthArr() {
-    //     const {initYearMonth} = this.state;
-    //     // 把initYear跟Month分開
-    //     const initYearMonthLen = initYearMonth.length;
-    //     const initYear = initYearMonth.substr(0, 4);
-    //     const initMonth = initYearMonth.substr(initYearMonthLen - 2, 2); // 只取後面兩個
-    //     let initMonthAfterZero;
-    //     // 如果initMonth第一個數字是0 ==>只取後面的數字
-
-    //     if (initMonth[0] == '0') {
-    //         //   initMonth = init[1];  // Uncaught Error: "initMonth" is read-only
-    //         initMonthAfterZero = initMonth[1];
-    //         // console.log(initMonthAfterZero);
-    //     } else {
-    //         initMonthAfterZero = initMonth;
-    //     }
-
-    //     console.log(initMonthAfterZero);
-    //     console.log(initYear);
-
-    //     let preInitYear;
-    //     let afterInitYear;
-    //     if (initMonthAfterZero === '1') {
-    //         console.log('initMonthAfterZero===1');
-    //         // console.log('initMonthAfterZero', initMonthAfterZero);
-    //         preInitYear = String(parseInt(initYear) - 1);
-    //         afterInitYear = initYear;
-    //         console.log(preInitYear);
-    //     } else if (initMonthAfterZero === '12') {
-    //         preInitYear = String(parseInt(initYear) - 1);
-
-    //         afterInitYear = initYear;
-    //         // preInitYear = initYear;
-    //     } else {
-    //         preInitYear = initYear;
-    //         afterInitYear = initYear;
-    //     }
-
-    //     const renderYearArr = [preInitYear, initYear, afterInitYear];
-
-    //     // 放進要render(map)的arr
-    //     // 待處理:  如果輸入的月份是01 => premonth變12
-    //     let preInitMonth;
-    //     if (initMonthAfterZero === '1') {
-    //         preInitMonth = '12';
-    //     } else {
-    //         preInitMonth = String(parseInt(initMonthAfterZero) - 1);
-    //     }
-    //     console.log('preInitMonth');
-    //     console.log(preInitMonth);
-
-    //     let afterInitMonth;
-    //     if (initMonthAfterZero === '12') {
-    //         afterInitMonth = '1';
-    //     } else {
-    //         afterInitMonth = String(parseInt(initMonthAfterZero) + 1);
-    //     }
-
-    //     console.log('afterInitMonth');
-    //     console.log(afterInitMonth);
-    //     const renderMonthArr = [preInitMonth, initMonthAfterZero, afterInitMonth];
-
-    //     this.setState({
-    //         renderMonthArr: renderMonthArr,
-    //         renderYearArr: renderYearArr,
-    //         initYear: initYear,
-    //         initMonthAfterZero: initMonthAfterZero,
-    //         preInitYear,
-    //         afterInitYear,
-    //     });
-
-    // // initYear,
-    // }
-    // 原本年月合起來的function 以上
-
     handleSlideClick(leftOrRight) {
         const {
             initYear,
             initMonthAfterZero,
+
             preInitYear,
             afterInitYear,
+
+            preInitMonth,
+            afterInitMonth,
         } = this.state;
         // 把initYear跟Month分開
         // const initYearMonthLen = initYearMonth.length;
@@ -265,60 +220,51 @@ class CalendarHead extends Component {
         if (leftOrRight == 'left') {
             console.log('initYear, initMonthAfterZero, preInitYear, afterInitYear');
             console.log(initYear, initMonthAfterZero, preInitYear, afterInitYear);
-        }
-    }
+            // if initMonthAfterZero == 1 ( 點左邊時 ) => initMonthAfterZero 變12
+            let slideLeftInitMonth;
+            let slideLeftPreInitMonth;
+            let slideLeftAfterInitMonth;
+            if (initMonthAfterZero === '12') {
+                console.log('if in leftClick');
+                slideLeftInitMonth = String(parseInt(initMonthAfterZero) - 1);
+                slideLeftPreInitMonth = String(parseInt(preInitMonth) - 1);
+                slideLeftAfterInitMonth = '12';
+            } else if (initMonthAfterZero === '1') {
+                console.log('else if in leftClick');
+                slideLeftInitMonth = '12';
+                slideLeftPreInitMonth = String(parseInt(preInitMonth) - 1);
+                slideLeftAfterInitMonth = String(parseInt(afterInitMonth) - 1);
+            } else {
+                console.log('else in leftClick');
+                console.log('else: initMonthAfterZero');
+                console.log(initMonthAfterZero);
+                console.log('slideLeftAfterInitMonth before -1');
+                console.log(slideLeftAfterInitMonth);
+                slideLeftInitMonth = String(parseInt(initMonthAfterZero) - 1);
+                console.log('slideLeftAfterInitMonth after -1');
+                console.log(slideLeftAfterInitMonth);
+                slideLeftPreInitMonth = String(parseInt(preInitMonth) - 1);
+                slideLeftAfterInitMonth = String(parseInt(afterInitMonth) - 1);
+            }
+            const leftSlideRenderMonthArr = [
+                slideLeftPreInitMonth,
+                slideLeftInitMonth,
+                slideLeftAfterInitMonth,
+            ];
 
-    getData(path) {
-        fetch(path)
-            .then((response) => {
-                return response.json();
-            })
-            .then((travelDataHead) =>
-                this.setState(
-                    {
-                        travelDataHead: travelDataHead,
-                        // }
-                    },
-                    () => {
-                        this.renderDataFunc(this.state.travelDataHead, 'date');
-                        console.log('hithere');
-                        // this.collectInitYearMonth();
-                        // this.handleMonthArr();
-                        // this.handleYearArr();
-                        // this.sortedDateFunc(this.state.unsortedArr);
-                        // console.log('this.state.unsortedArr');
-                        // console.log(this.state.unsortedArr);
-                        // console.log('travelDataHead in getData');
-                        // console.log(this.state.travelDataHead);
-                    }
-                )
-            )
-            .catch((error) => console.log('parsing failed', error));
+            this.setState({
+                initMonthAfterZero: slideLeftAfterInitMonth,
+                preInitMonth: slideLeftPreInitMonth,
+                afterInitMonth: slideLeftAfterInitMonth,
+                renderMonthArr: leftSlideRenderMonthArr,
+            });
+        }
     }
 
     // componentWillMount() {
     //     console.log('willMount');
     //     this.getData(this.props.path); // fetch data
     // }
-
-    componentDidMount() {
-        const {travelDataHead} = this.state;
-        // this.handleRenderYearMonthArr();
-        this.getData(this.props.path);
-        this.collectInitYearMonth();
-
-        // this.handleYearArr();
-        // this.handleMonthArr();
-        // this.renderDataFunc(travelDataHead);
-
-        // console.log('travelDataHead in DidMount');
-        // console.log(travelDataHead); // 這邊為何拿不到東西?
-        // console.log('this.state.oldestMonth in DidMount');
-        // console.log(this.state.oldestMonth);
-        // console.log(this.state.unsortedArr);
-
-    // this.sortedDateFunc(this.state.unsortedArr);
-    }
 
     render() {
         const {
@@ -331,11 +277,14 @@ class CalendarHead extends Component {
         } = this.state;
         console.log('init Render');
         console.log('bf if', travelDataHead);
-        this.handleYearArr;
+        // this.handleYearArr;
+        // console.log('preInitMonth');
+        // console.log(this.state.preInitMonth);
         if (travelDataHead) {
-            console.log('initYearinitYear789', this.state.initYear);
+            // console.log('initYearinitYear789', this.state.initYear);
 
-            console.log('initMonthAfterZero789', this.state.initMonthAfterZero);
+            // console.log('initMonthAfterZero789', this.state.initMonthAfterZero);
+            console.log('999', this.state);
             // this.renderStateFunc();
             // console.log('render in if travelData');
             // console.log('this.state.unsortedArr');
